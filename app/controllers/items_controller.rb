db275cfbaa9e4c3c16bf96529868fd93457ef30a
keiichi031605 <keichi031605@gmail.com>
2022-04-19 08:53:36 +1000

    added logo image

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,5 @@ class ItemsController < ApplicationController
   
   def index
     @items = Item.all
-    # @current_user = User.find(params[:id])
-    puts "###" * 100
-    puts params
-    puts "###" * 100
   end
 end
